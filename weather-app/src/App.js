b002bd173bbd19d9f3486175fe653d69b06bf51f
Yana <kryshchuk@ucu.edu.ua>
2020-11-05 11:05:43 +0200

    Initial commit

---
+++ weather-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Forecast from "./components/Forecast/Forecast";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather App</h1>
+      </header>
+      <main>
+        <Forecast />
+      </main>
+      <footer>Page created by Yana Kryshchuk</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-app/src/components/Conditions/Conditions.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './Conditions.module.css'
+const conditions = (props) => {
+   return (
+       <div className={classes.Wrapper}>
+           {props.error && <small className={classes.Small}>Please enter a valid city.</small>}
+           {props.loading && <div className={classes.Loader} />}
+           {props.responseObj.cod === 200 ?
+               <div>
+                   <p>City: <strong>{props.responseObj.name}</strong></p>
+                   <p>Temperature: <strong>{Math.round(props.responseObj.main.temp)} degrees</strong> </p>
+                   <p>Weather conditions: <strong>{props.responseObj.weather[0].description}</strong></p>
+                   <p>Humidity: <strong>{props.responseObj.main.humidity}</strong></p>
+                   <p>Pressure: <strong>{props.responseObj.main.pressure} hPa </strong></p>
+                   <p>Wind speed: <strong>{props.responseObj.wind.speed} m/s </strong></p>
+               </div>
+           : null
+           }
+       </div>
+   )
+}
+export default conditions;
